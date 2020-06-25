5764ba4b1202033204e6c165789b4cd79bd32bc8
David Ben-Yaakov <david@tangogolfdigital.com>
2020-06-24 21:36:02 -0500

    Corrected copy of symlink as link

---
+++ README.md
@@ -0,0 +1,1 @@
+A collection of humorous and/or constructive quotes for "fortune".
--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "Deploying new files"
 for file in $(ls -1 --ignore=*.sh --ignore=*~)
 do
     targetFile="/usr/share/games/fortunes/$file"
-    cp --preserve=links "$file" "$targetFile"
+    cp -P "$file" "$targetFile"
     chmod a+r "$targetFile"
 done
 
