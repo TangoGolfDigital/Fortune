b00f7e4f3dbab11adf9a91b5cf3b3df77c693368
David Ben-Yaakov <david@tangogolfdigital.com>
2020-06-24 22:18:11 -0500

    Added check for newline at EOF

--- make.sh
+++ make.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+newline_at_eof()
+{
+    if [ -z "$(tail -c 1 "$1")" ]
+    then
+	return 1 # No newline at end of file
+    else
+	return 0 # Newline at end of file
+    fi
+}
+
 # Remove all previously generated files
 echo "Removing previously generated files"
 rm *.u8 *.dat
@@ -9,7 +19,13 @@ rm *.u8 *.dat
 echo "Generating new files..."
 for file in $(ls -1 --ignore='*.*' --ignore='*~')
 do
-    echo $file
+    echo "Checking $file..."
+    if newline_at_eof "$file" -gt 0
+    then
+	echo "Missing newline at end of $file"
+	exit 1
+    fi
+    
     strfile $file
     ln -s "$file" "$file.u8"
 done
